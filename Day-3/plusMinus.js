553e442b682ed68f8022ddd23e2509f945855248
Christian Fernandez <christian14@gmail.com>
2023-11-08 07:40:52 -0300

    solucion 1 fn plusMinus

---
+++ Day-3/plusMinus.js
@@ -0,0 +1,34 @@
+/* 
+Este código calcula la proporción de números positivos, negativos y ceros en el array y luego imprime los resultados con 6 decimales. 
+*/
+
+const plusMinus = (arr) => {
+
+  let positiveCount = 0;
+  let negativeCount = 0;
+  let zeroCount = 0;
+
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+
+    if (arr[i] > 0) {
+      positiveCount++;
+    } else if (arr[i] < 0) {
+      negativeCount++;
+    } else {
+      zeroCount++
+    }
+  }
+  
+  const positiveRatio = positiveCount / n;
+  const negativeRatio = negativeCount / n;
+  const zeroRatio = zeroCount / n;
+
+  console.log(positiveRatio.toFixed(6));
+  console.log(negativeRatio.toFixed(6));
+  console.log(zeroRatio.toFixed(6));
+}
+
+const array = [-4, 5, -9, 0, 4, 1]
+plusMinus(array);
